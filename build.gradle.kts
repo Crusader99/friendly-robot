836fdb1671db0408c608a2bedfa7b5ab0412df27
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2020-03-12 20:48:37 +0100

    Add logback-classic and args-library

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,21 @@ repositories {
 }
 
 dependencies {
+    // Kotlin standard library
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.70")
+
+    // Log management
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+
+    // Multi-platform library for shorter access to some java api functionality
     implementation("de.crusader:kotlin-extensions-jvm:1.0.0")
+
+    // Kotlin based multi-plattform projekt provides Color, Rectangle, Point, etc
     implementation("de.crusader:library-objects-jvm:1.0.0")
-    implementation("org.languagetool:language-de:4.8")
+
+    // Easy to use command line argument parser for kotlin
+    implementation("de.crusader:library-args:1.0.0")
+
+    // Language tool allows style and grammar checking
+    implementation("org.languagetool:language-all:4.8")
 }
